353264a046a0cccc2997822259b341725b685309
FraanBat <friggio.87@gmail.com>
2024-04-21 12:58:36 -0300

    Filtrado de especialistas dentro de la sección de busqueda

--- static/js/servicios.js
+++ static/js/servicios.js
@@ -1,13 +1,52 @@
+let listaEspecialistas = []
+
+
+//Solicita a la API el listado con todos los especialistas y lo guarda
 const solicitarEspecialistas = () => {
     fetch(`https://api.mockaroo.com/api/9d323580?count=80&key=b59cfd90`)
     .then(response => response.json())
     .then(data => new Promise(() => {
         sessionStorage.setItem("listadoEspecialistas", JSON.stringify(data))
+        listaEspecialistas = data
         mostrarEspecialistas(data)
     }))
 }
 
+const filtroEspecialistas = (listaEspecialistas, filtroEspecialista) => {
+    if(filtroEspecialista !== ""){
+        listaEspecialistas = listaEspecialistas.filter(especialista => especialista.profesion === filtroEspecialista)
+    }
+    return listaEspecialistas
+}
+
+document.getElementById("buscadorEspecialidad").addEventListener('submit', function(event){
+    event.preventDefault()
+
+    let especialidad = document.getElementById("especialidad").value
+    let listaEspecialistasFiltrada = filtroEspecialistas(listaEspecialistas, especialidad)
+    if(listaEspecialistasFiltrada.length > 0){
+        mostrarEspecialistas(listaEspecialistasFiltrada)
+    }
+    else{
+        Swal.fire({
+            title: "Sin stock",
+            text: "Lo siento, pero no se encontraron especialistas con las especificaciones realizadas",
+            icon: "warning",
+            confirmButtonColor: "#356194",
+            confirmButtonText: "Aceptar"
+          });
+    }
+})
+
+//Muestra los especialistas, de acuerdo con las especificaciones realizadas
 const mostrarEspecialistas = listadoEspecialistas => {
+
+    //Verifica si hay que limpiar el listado de especialistas que muestra actualmente
+    const limpiarListado = document.querySelectorAll("div.especialista")
+    if(limpiarListado.length > 0){
+        limpiarListado.forEach(especialista => especialista.remove())
+    }
+
     const especialistas = document.getElementById("especialistas")
     const especialistaDisponible = []
     listadoEspecialistas.forEach(especialista => {
