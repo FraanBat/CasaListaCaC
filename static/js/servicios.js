f4e1eac6c7ac6d542fde937e44b35b47f626262a
OjedaEduardo <eduardo.ojeda.1293@gmail.com>
2024-04-23 15:52:56 -0300

    cam

--- static/js/servicios.js
+++ static/js/servicios.js
@@ -4,37 +4,37 @@ let listaEspecialistas = []
 //Solicita a la API el listado con todos los especialistas y lo guarda
 const solicitarEspecialistas = () => {
     fetch(`https://api.mockaroo.com/api/9d323580?count=80&key=b59cfd90`)
-    .then(response => response.json())
-    .then(data => new Promise(() => {
-        sessionStorage.setItem("listadoEspecialistas", JSON.stringify(data))
-        listaEspecialistas = data
-        mostrarEspecialistas(data)
-    }))
+        .then(response => response.json())
+        .then(data => new Promise(() => {
+            sessionStorage.setItem("listadoEspecialistas", JSON.stringify(data))
+            listaEspecialistas = data
+            mostrarEspecialistas(data)
+        }))
 }
 
 const filtroEspecialistas = (listaEspecialistas, filtroEspecialista) => {
-    if(filtroEspecialista !== ""){
+    if (filtroEspecialista !== "") {
         listaEspecialistas = listaEspecialistas.filter(especialista => especialista.profesion === filtroEspecialista)
     }
     return listaEspecialistas
 }
 
-document.getElementById("buscadorEspecialidad").addEventListener('submit', function(event){
+document.getElementById("buscadorEspecialidad").addEventListener('submit', function (event) {
     event.preventDefault()
 
     let especialidad = document.getElementById("especialidad").value
     let listaEspecialistasFiltrada = filtroEspecialistas(listaEspecialistas, especialidad)
-    if(listaEspecialistasFiltrada.length > 0){
+    if (listaEspecialistasFiltrada.length > 0) {
         mostrarEspecialistas(listaEspecialistasFiltrada)
     }
-    else{
+    else {
         Swal.fire({
             title: "Sin disponibilidad",
             text: "Lo siento, pero no se encontraron especialistas con las especificaciones realizadas",
             icon: "warning",
             confirmButtonColor: "#356194",
             confirmButtonText: "Aceptar"
-          });
+        });
     }
 })
 
@@ -43,7 +43,7 @@ const mostrarEspecialistas = listadoEspecialistas => {
 
     //Verifica si hay que limpiar el listado de especialistas que muestra actualmente
     const limpiarListado = document.querySelectorAll("div.especialista")
-    if(limpiarListado.length > 0){
+    if (limpiarListado.length > 0) {
         limpiarListado.forEach(especialista => especialista.remove())
     }
 
@@ -52,19 +52,19 @@ const mostrarEspecialistas = listadoEspecialistas => {
     listadoEspecialistas.forEach(especialista => {
         const nuevoEspecialista = document.createElement("div")
         nuevoEspecialista.className = "especialista"
-        nuevoEspecialista.innerHTML = 
-        `
+        nuevoEspecialista.innerHTML =
+            `
         <a class="linea" href="detalle.html?id_especialista=${especialista.id}">
             <div>
                 <img class="foto" src="${especialista.foto_perfil}" alt="">
             </div>
             <div>
                 <br> <br><br>
-                <a href="detalle.html?id_especialista=${especialista.id}">
+            
                     <h4>${especialista.profesion}<br>
                         ${especialista.apellido} ${especialista.nombre}
                     </h4>
-                </a>
+                
             </div>
             <div class="descripcion">
                 <br>
