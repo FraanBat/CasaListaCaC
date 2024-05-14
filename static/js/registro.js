d96d6bfeeef17277c8e89f61f399a67f26ef9dc4
FraanBat <friggio.87@gmail.com>
2024-05-14 00:53:58 -0300

    Actualización fakepath v3

--- static/js/registro.js
+++ static/js/registro.js
@@ -65,7 +65,7 @@ document.getElementById("altaUsuario").addEventListener('submit', function (even
                 zona: document.getElementById("zona").value,
                 telefono: document.getElementById("telefono").value,
                 genero: document.getElementById("genero").value,
-                imagen: document.getElementById("imagen").value.replace('C:\\fakepath\\'),
+                imagen: document.getElementById("imagen").value,
                 especializacion: {
                     especialista: false,
                     profesion: null
