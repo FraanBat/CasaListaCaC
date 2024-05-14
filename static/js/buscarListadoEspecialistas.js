f076b7f997b3d19c3c972154069938c5aea2fa80
FraanBat <friggio.87@gmail.com>
2024-05-14 12:02:15 -0300

    cambio API expiración

--- static/js/buscarListadoEspecialistas.js
+++ static/js/buscarListadoEspecialistas.js
@@ -1,6 +1,6 @@
 //Solicita a la API el listado con todos los especialistas y lo guarda
 const solicitarEspecialistas = function() {
-    fetch(`https://api.mockaroo.com/api/9d323580?count=80&key=b59cfd90`)
+    fetch(`https://api.mockaroo.com/api/e6a3a530?count=80&key=df8e15e0`)
     .then(response => response.json())
     .then(data => {
         sessionStorage.setItem("listadoEspecialistas", JSON.stringify(data))
